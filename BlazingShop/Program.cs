dea7c374175505044f8e9714c8fc43fb636f44da
carlosalberto_filho <carlosalberto_filho@outlook.com>
2024-05-01 23:36:17 +0000

    Corrigir configurações do contêiner de desenvolvimento

--- BlazingShop/Program.cs
+++ BlazingShop/Program.cs
@@ -1,27 +1,52 @@
 using BlazingShop.Components;
+using Microsoft.AspNetCore.HttpsPolicy;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builderAddComponents(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+IsDevelopment(app);
 
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+appAddComponents(app);
 
 app.Run();
+
+static void builderAddComponents(WebApplicationBuilder builder)
+{
+    // Configure o caminho do certificado SSL
+    builder.WebHost.UseKestrel(options =>
+    {
+        options.ListenAnyIP(5001, listenOptions =>
+        {
+            listenOptions.UseHttps("/workspaces/balta-blazor-server/.aspnet/https/aspnetapp.pfx",
+            "SecurePwdGoesHere");
+        });
+    });
+    // Add services to the container.
+    builder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+}
+
+static void IsDevelopment(WebApplication app)
+{
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+}
+
+static void appAddComponents(WebApplication app)
+{
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+}
