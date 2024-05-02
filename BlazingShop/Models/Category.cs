5350e2ca1e726057431dff74707776b5e9af959e
carlosalberto_filho <carlosalberto_filho@outlook.com>
2024-05-02 00:52:24 +0000

    #Models: add Category and Product

---
+++ BlazingShop/Models/Category.cs
@@ -0,0 +1,7 @@
+namespace BlazingShop.Models;
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+}
